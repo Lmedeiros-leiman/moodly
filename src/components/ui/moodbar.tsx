61caeeca5148a16c8d2c959e818482db77ce1195
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-22 19:05:57 -0300

    added new moodbar component

---
+++ src/components/ui/moodbar.tsx
@@ -0,0 +1,23 @@
+import { AvailableMoods, MoodColors, MoodKey } from "@/lib/moods";
+
+
+
+interface Props {
+   mood?: MoodKey
+   day?: string
+}
+
+export default function Moodbar({ mood = "None" }: Props) {
+   if (typeof mood == undefined || mood == null || !(mood in AvailableMoods)) {
+      mood = "None";
+   }
+
+   const className = MoodColors[mood]
+   const text =  (AvailableMoods[mood]).split(" ")
+   
+
+   return (<div className={` py-3.5 outline outline-1 outline-zinc-400 shadow-lg rounded-sm ${className}`}>
+      <span className="hidden md:inline">{text[0]}</span>
+      <span>{text[1]}</span>
+   </div>)
+}
