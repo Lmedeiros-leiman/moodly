10bd5a25b6a3cc5b894794f1a26540df567aca35
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-22 22:06:12 -0300

    added dropdown

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,20 +1,87 @@
-import React from "react"
+'use client'
+import React, { useEffect, useState } from "react"
+import { createClient } from "@/lib/Supabase/components";
+import { Session } from "@supabase/supabase-js";
+
+
+const authContext = React.createContext<AuthContextType>({
+   Session: null,
+   setSession: () => { },
+   busy: true,
+   setBusy: () => { },
+});
+const supabase = createClient();
 
 
 
-const authContext = React.createContext({});
+export type authRequest = {
+   email: string;
+   password: string;
+   remenberUser?: boolean;
+}
 
+export async function SignUserIn(authData : authRequest) {
+   const response = await supabase.auth.signInWithPassword({
+      password: authData.password,
+      email: authData.email,
+   });
+   
+   return response
+}
+
+export async function SignUserUp(authData : authRequest) {
+   const response = await supabase.auth.signUp({
+      password: authData.password,
+      email: authData.email,
+   });
+   return response
+}
+
+export async function SignInOAuth() {
+   return await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+         redirectTo: "./dashboard",
+         queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+         },
+      },
+   });
+}
+
+
+type AuthContextType = {
+   Session: Session | null;
+   setSession: React.Dispatch<React.SetStateAction<Session | null>>;
+   busy: boolean;
+   setBusy: React.Dispatch<React.SetStateAction<boolean>>;
+}
 export const UseAuth = () => {
-   return React.useContext(authContext);
+   return React.useContext<AuthContextType>(authContext);
 }
 
 export const AuthProvider = ({children}: {children: React.ReactNode}) => {
    
-   
+   const [Session, setSession] = useState< Session | null>(null);
+   const [busy,setBusy] = useState(true);
+
+   useEffect( () => {
+      async function checkUser() {
+         // automatically fetches the cached user/ user session
+         const {data} = await supabase.auth.getSession();
+         if (data) { setSession(data.session);}
+         
+      }
+
+      
+      checkUser().finally( () => setBusy(false));
+   },[]);
 
 
    const value = {
-      
+      Session, setSession,
+      busy, setBusy,
    }
    
    return (
@@ -23,3 +90,4 @@ export const AuthProvider = ({children}: {children: React.ReactNode}) => {
       </authContext.Provider>
    )
 }
+
